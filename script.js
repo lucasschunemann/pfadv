bd6f815d50c9ffa92e07d77686f213bd5a8c9797
Lucas Schünemann <88516015+lucasschunemann@users.noreply.github.com>
2023-12-08 09:46:49 -0300

    Alterações para FAQs

--- script.js
+++ script.js
@@ -1,42 +1,56 @@
 function scrollToElement(elementSelector, instance = 0) {
-    // Select all elements that match the given selector
-    const elements = document.querySelectorAll(elementSelector);
-    // Check if there are elements matching the selector and if the requested instance exists
-    if (elements.length > instance) {
-        // Scroll to the specified instance of the element
-        elements[instance].scrollIntoView({ behavior: 'smooth' });
-    }
+  // Select all elements that match the given selector
+  const elements = document.querySelectorAll(elementSelector);
+  // Check if there are elements matching the selector and if the requested instance exists
+  if (elements.length > instance) {
+    // Scroll to the specified instance of the element
+    elements[instance].scrollIntoView({ behavior: "smooth" });
+  }
 }
 
-window.addEventListener("scroll", function(){
-    let headermotion = document.querySelector('#headermotion')
-    headermotion.classList.toggle('rolagem', window.scrollY > 0)
-})
+window.addEventListener("scroll", function () {
+  let headermotion = document.querySelector("#headermotion");
+  headermotion.classList.toggle("rolagem", window.scrollY > 0);
+});
 
 const link1 = document.getElementById("link1");
 const link2 = document.getElementById("link2");
 const link3 = document.getElementById("link3");
 
-link1.addEventListener('click', () => {
-    scrollToElement('.header');
+link1.addEventListener("click", () => {
+  scrollToElement(".header");
 });
 
-link2.addEventListener('click', () => {
-    // Scroll to the second element with "header" class
-    scrollToElement('.header', 1);
+link2.addEventListener("click", () => {
+  // Scroll to the second element with "header" class
+  scrollToElement(".header", 1);
 });
 
-link3.addEventListener('click', () => {
-    scrollToElement('.header', 2);
+link3.addEventListener("click", () => {
+  scrollToElement(".header", 2);
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    const faqButtons = document.querySelectorAll('.faq .btn');
+const accordion = document.querySelector(".accordion");
 
-    faqButtons.forEach(button => {
-        button.addEventListener('click', function () {
-            const content = this.parentElement.querySelector('.content');
-            content.style.display = content.style.display === 'block' ? 'none' : 'block';
-        });
-    });
+accordion.addEventListener("click", (e) => {
+  e.target.classList.toggle("active");
+  e.target.nextElementSibling.classList.toggle("collapsed");
 });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    this.parentElement.classList.toggle("active");
+
+    var pannel = this.nextElementSibling;
+
+    if (pannel.style.display === "block") {
+      pannel.style.display = "none";
+    } else {
+      pannel.style.display = "block";
+    }
+  });
+}
